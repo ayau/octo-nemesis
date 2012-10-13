0f88f16346f69e549992f382c5673fd25875dc2d
Brandon Krieger <brandontkrieger@gmail.com>
2012-10-13 17:33:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RushApp::Application.config.secret_token = '3b3929fa6bf41f023432f8c5779846b4b8cf86d17ec308b268a53f6fe74e2ef45cfe209c78028892a9afd0e47ac5348d63547c7b15b7fea4e2bded064eed556a'
