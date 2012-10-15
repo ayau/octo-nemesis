c0bf4705dd0fc6826718ae43ea9ef64ce57afeed
Brandon Krieger <brandontkrieger@gmail.com>
2012-10-15 18:33:03 -0400

    cant add blank comment

--- app/controllers/rush_comments_controller.rb
+++ app/controllers/rush_comments_controller.rb
@@ -4,11 +4,8 @@ class RushCommentsController < ApplicationController
 
   def create
   	@rush_comment = RushComment.new(params[:rush_comment])
-        if @rush_comment.save
-            redirect_to Rush.find(@rush_comment.rush_id)
-        else
-            render 'new'
-        end
+    @rush_comment.save
+    redirect_to Rush.find(@rush_comment.rush_id)
   end
 
   def destroy
