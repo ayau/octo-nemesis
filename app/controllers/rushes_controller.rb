ca6bfbcf7b7265d2edf7d87f1d54a683b021cc89
Brandon Krieger <brandontkrieger@gmail.com>
2012-10-15 23:44:20 -0400

    Made it so phone numbers are stored as 10 digit strings but then displayed with formatting

--- app/controllers/rushes_controller.rb
+++ app/controllers/rushes_controller.rb
@@ -4,10 +4,15 @@ class RushesController < ApplicationController
     end
     def create
         @rush = Rush.new(params[:rush])
-        if @rush.save
-            redirect_to @rush
-        elsif Rush.find_by_phone(@rush.phone)
-            redirect_to Rush.find_by_phone(@rush.phone)
+        @rush.phone = @rush.phone.gsub(/[^0-9]/i, '')
+        if (@rush.phone.length == 10)
+            if @rush.save
+                redirect_to @rush
+            elsif Rush.find_by_phone(@rush.phone)
+                redirect_to Rush.find_by_phone(@rush.phone)
+            else
+                render 'new'
+            end
         else
             render 'new'
         end
