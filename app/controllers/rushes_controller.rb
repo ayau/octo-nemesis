a1e3f7ea9a3edf670c570c0ed5facc7031eae91a
Brandon Krieger <brandontkrieger@gmail.com>
2012-10-24 23:46:47 -0400

    fix 2 decimal point chill pull

--- app/controllers/rushes_controller.rb
+++ app/controllers/rushes_controller.rb
@@ -39,7 +39,7 @@ class RushesController < ApplicationController
 
     def index
         @rushes = Rush.find_by_sql "select * from rushes as r 
-                                    left join (select rush_id, COALESCE(avg(chill),0) as avg_chill from cpratings group by rush_id) as c 
+                                    left join (select rush_id, COALESCE(avg(chill),0.0) as avg_chill from cpratings group by rush_id) as c 
                                         on c.rush_id = r.id 
                                         order by c.avg_chill DESC"
         @RushesController = self
