5bbdf335c2037747ed6c895020ca374e821106e3
Brandon Krieger <brandontkrieger@gmail.com>
2013-01-02 12:16:31 -0500

    all photos can be viewed from dropdown on rush page, js done too

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,21 @@
 //= require jquery_ujs
 //= require_tree ./global
 //= require jquery.ui.all
+
+function changePhoto(numPhotos,hasOld) {
+	var dropdown=document.getElementById("photo_dropdown");
+	var photo_number=dropdown.options[dropdown.selectedIndex].text;
+	for (i=1; i<=numPhotos; i++) {
+    	var temp_id = "profile_picture_" + i;
+    	document.getElementById(temp_id).style.display = "none";
+	}
+	if (hasOld) {
+		document.getElementById("profile_picture_old").style.display = "none";
+	}
+	if (photo_number > numPhotos) {
+		document.getElementById("profile_picture_old").style.display = "block";
+	} else {
+		var active_id = "profile_picture_" + photo_number;
+		document.getElementById(active_id).style.display = "block";
+	}
+}
