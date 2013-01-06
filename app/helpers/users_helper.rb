d109d77e9775d876754bb3345d547de528e08569
Alex Yau <ayau@sas.upenn.edu>
2013-01-06 18:05:48 +0800

    uncommented koala

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,15 @@
 module UsersHelper
 
 	def getPictureForName(user_token,rush_name)
-        # graph = Koala::Facebook::GraphAPI.new(user_token)
-        # facebook_friends = graph.get_connections("me", "friends", :fields =>"name,picture.type(large)")
-        # facebook_friends.each do |fa|
-        #     if (fa["name"].downcase == rush_name.downcase)
-        #         return fa["picture"]["data"]["url"]
-        #     end
-        # end
+        graph = Koala::Facebook::GraphAPI.new(user_token)
+        facebook_friends = graph.get_connections("me", "friends", :fields =>"name,picture.type(large)")
+        facebook_friends.each do |fa|
+            if (fa["name"].downcase == rush_name.downcase)
+                return fa["picture"]["data"]["url"]
+            end
+        end
 
-        # Return blank string if not friends
+        Return blank string if not friends
         return ""
     end
 
